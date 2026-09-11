2d7a43cdfd18ffdbcdd2bd9916cf84480edfa9fe
Renato Vassão <renatomvd@hotmail.com>
2026-09-11 16:24:31 -0300

    wrong sum

--- tests/unit.test.ts
+++ tests/unit.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 import { sum } from '../src/lib.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 2', () => {
+  expect(sum(1, 2)).toBe(2);
 });
